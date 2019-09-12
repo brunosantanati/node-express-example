bef82abdeca4b10ad2e4d684383819a862acab5d
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-09-12 18:46:17 -0300

    Implemented dynamic pages with templating(hbs)

--- src/app.js
+++ src/app.js
@@ -6,9 +6,28 @@ console.log(__filename)
 console.log(path.join(__dirname, '../public'))*/
 
 const app = express()
-
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('/help', (req, res) => {
+    res.render('help', { helpText: 'This is the help page' })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Bruno'
+    })
+})
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather app',
+        name: 'Bruno'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([
