5847e9360e44983c0b12770b99c4928192d1e296
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-09-13 18:05:33 -0300

    Implemented Partials

--- src/app.js
+++ src/app.js
@@ -1,25 +1,28 @@
 const path = require('path')
 const express = require('express')
-
-/*console.log(__dirname)
-console.log(__filename)
-console.log(path.join(__dirname, '../public'))*/
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('/help', (req, res) => {
-    res.render('help', { helpText: 'This is the help page' })
+    res.render('help', { 
+        helpText: 'This is the help page',
+        title: 'Help',
+        name: 'Bruno'
+    })
 })
 
 app.get('/about', (req, res) => {
