0ad0366a9e28c9981ad02d0fa350ec47430372a4
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-04-04 17:27:29 -0300

    Make HTTP request using fetch.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,18 @@
 console.log('Client side javascript file is loaded!')
+
+fetch('http://puzzle.mead.io/puzzle').then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+fetch('http://localhost:3000/weather?address=%22Santo%20Andr%C3%A9%22').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+})
