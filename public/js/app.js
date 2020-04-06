8c30a1aa7326b514f00e238e26628ca3c8aff094
Bruno Rafael Sant' Ana <bruno.santana.ti@gmail.com>
2020-04-06 20:07:49 -0300

    Create form to search for addresses.

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,22 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
 
-fetch('http://localhost:3000/weather?address=%22Santo%20Andr%C3%A9%22').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-        }else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+weatherForm.addEventListener('submit', (e) => {
+    
+    e.preventDefault()
+
+    const location = search.value
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                console.log(data.error)
+            }else{
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
 })
